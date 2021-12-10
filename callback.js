8a19966aac5cdbadd0ff32acb33e2a1fa73820ed
Isaac Molina <izaac.everaldo@gmail.com>
2021-12-09 23:31:14 -0600

    se hizo merge

--- callback.js
+++ callback.js
@@ -1,4 +1,5 @@
 function SoyUnMetodoAsync(){
     console.log("Hola, soy una funcion asíncrona ");
     console.log("Se agrego una linea remota");
+    console.log("se agrego una linea en repo local");
 }
